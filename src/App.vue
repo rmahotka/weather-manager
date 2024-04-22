ed19c47263831371a463e23653d848d3fc586991
rmahotka <rmahotka@gmail.com>
2024-04-22 23:06:32 +0300

    add icon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <div></div>
+  <main class="main rounded-xl">
+    <WeatherSummary />
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
 
-<style lang="scss" scoped></style>
+import WeatherSummary from './components/WeatherSummary.vue'
+</script>
+
+<style lang="scss" scoped>
+.main {
+  width: 1000px;
+  height: 500px;
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
+  background-color: rgba(255, 255, 255, 0.15);
+  backdrop-filter: blur(5px);
+}
+</style>
