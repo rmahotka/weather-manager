f963dc232d0ec72c21d829bbb49ddee929a13c7e
mahotich <rmahotka@gmail.com>
2024-04-24 15:14:26 +0300

    test

--- src/components/CardInfoWeather.vue
+++ src/components/CardInfoWeather.vue
@@ -1,16 +1,20 @@
 <script setup>
 defineProps({
-  itemInfo: Object,
-  weatherInfo: Object
+  itemInfo: Object
 })
 </script>
 
 <template>
-  <div class="bg-white p-5 rounded-xl w-52">
+  <div class="itemCard bg-white p-5 rounded-xl w-52">
     <p>{{ itemInfo.title }}</p>
     <div v-html="itemInfo.icon"></div>
-    <p>{{ itemInfo.text }}</p>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.itemCard {
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(5px);
+  background-color: rgb(255 255 255 / 50%);
+}
+</style>
